f805e2c0b415759c33d83cda13e6a21ed0db60da
Prabhakar Kumar <pk912873@gmail.com>
2025-05-22 18:11:32 +0530

    Check duck number program

---
+++ 13.js
@@ -0,0 +1,8 @@
+// check duck number program
+if (num[0] === '0') {
+  console.log("Not a duck number");
+} else if (num.includes('0')) {
+  console.log("Duck number");
+} else {
+  console.log("Not a duck number");
+}
