1aab622ff02efb14d526b4614bbcab7b8a165b3e
Prabhakar Kumar <pk912873@gmail.com>
2025-05-30 10:54:45 +0530

    Implement perfect number logic in JavaScript

--- 45.js
+++ 45.js
@@ -4,3 +4,19 @@
 // A perfect number is a number that can be written as sum of its factors. 
 // (equal to sum of its proper divisors) excluding the number itself.
 
+let num = Number(prompt("Enter a number: "))
+function perfectNumber(num){
+    let sum = 0;
+    for(let i=1; i<Math.floor(num/2)+1; i++){
+        if(num%i==0){
+            sum += i;
+        }
+    }
+    if(num === sum){
+        return 'Perfect number'
+    }else{
+        return 'Not a perfect number'
+    }
+}
+
+console.log(perfectNumber(num));
