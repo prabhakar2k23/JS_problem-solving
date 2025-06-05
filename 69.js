0e4d3cbf905850cf7f7267d98f4440b32d749e8c
Prabhakar Kumar <pk912873@gmail.com>
2025-06-05 17:17:23 +0530

    count vowels

---
+++ 69.js
@@ -0,0 +1,15 @@
+// count vowels
+function countVowels(str) {
+  const vowels = "aeiouAEIOU";
+  let count = 0;
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countVowels("I love coding"));  
